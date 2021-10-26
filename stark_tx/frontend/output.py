3b6803b7761c31e5778fa74c55ab79991b09e051
Karol Chojnowski <karolchojnowski95@gmail.com>
2021-10-26 13:09:27 +0200

    use logger

--- stark_tx/frontend/output.py
+++ stark_tx/frontend/output.py
@@ -1,8 +1,17 @@
+import logging
+
+log = logging.getLogger(__name__)
+
+
 def print_transaction(tx):
-    print()
-    print(f"Block: {tx['block_id']}/{tx['block_number']} Time: {tx['timestamp']}")
-    print(
-        f"Tx: {tx['transaction_id']} Index: {tx['transaction_index']} Status: {tx['status']}"
+    log.info(
+        "Block: %d/%d Time: %s", tx["block_id"], tx["block_number"], tx["timestamp"]
+    )
+    log.info(
+        "Tx: %d Index: %d Status: %s",
+        tx["transaction_id"],
+        tx["transaction_index"],
+        tx["status"],
     )
     if tx["type"] == "INVOKE_FUNCTION":
         input_string = ", ".join(
@@ -11,15 +20,22 @@ def print_transaction(tx):
         output_string = ", ".join(
             [f"{_output['name']}={_output['value']}" for _output in tx["outputs"]]
         )
-        print(
-            f"Invoke {tx['contract_name']}.{tx['function']}({input_string}) -> ({output_string})"
+        log.info(
+            "Invoke %s.%s(%s) -> (%s)",
+            tx["contract_name"],
+            tx["function"],
+            input_string,
+            output_string,
         )
     elif tx["type"] == "DEPLOY":
-        print(f"Deploy {tx['contract']}")
+        log.info("Deploy %s", tx["contract"])
     for transaction in tx["l2_to_l1"]:
         payload_string = ", ".join(transaction["payload"])
-        print(
-            f"L2->L1: {transaction['from_address']} -> {transaction['to_address']} ({payload_string})"
+        log.info(
+            "L2->L1: %s -> %s (%s)",
+            transaction["from_address"],
+            transaction["to_address"],
+            payload_string,
         )
 
     return
