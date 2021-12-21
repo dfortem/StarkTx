49390ee1a6e60951c77ff6172d7e3c77553c3fcd
Karol Chojnowski <karolchojnowski95@gmail.com>
2021-12-21 10:47:07 +0100

    black

--- stark_tx/app/app/engine/decoders/parameter.py
+++ stark_tx/app/app/engine/decoders/parameter.py
@@ -14,24 +14,35 @@ def decode_parameters(parameters, parameters_abi):
             else parameters_abi[abi_index]["type"]
         )
 
-        if parameter_type == 'struct':
+        if parameter_type == "struct":
             name = parameters_abi[abi_index]["name"]
-            value = '{' + create_parameters_string(decode_struct(parameters[parameters_index:],
-                                                   parameters_abi[abi_index]['struct_members'])) + '}'
-            parameters_index += len(parameters_abi[abi_index]['struct_members'])
+            value = (
+                "{"
+                + create_parameters_string(
+                    decode_struct(
+                        parameters[parameters_index:],
+                        parameters_abi[abi_index]["struct_members"],
+                    )
+                )
+                + "}"
+            )
+            parameters_index += len(parameters_abi[abi_index]["struct_members"])
             abi_index += 1
         else:
-            value = decode_atomic_parameter(parameters[parameters_index], parameter_type)
+            value = decode_atomic_parameter(
+                parameters[parameters_index], parameter_type
+            )
             if (
                 abi_index + 1 < len(parameters_abi)
                 and parameters_abi[abi_index + 1]["type"] == "felt*"
-                and parameters_abi[abi_index]["name"] == parameters_abi[abi_index + 1]["name"] + "_len"
+                and parameters_abi[abi_index]["name"]
+                == parameters_abi[abi_index + 1]["name"] + "_len"
             ):
                 array_len = value
                 value = [
                     array_element
                     for array_element in parameters[
-                        parameters_index + 1: parameters_index + array_len + 1
+                        parameters_index + 1 : parameters_index + array_len + 1
                     ]
                 ]
                 name = parameters_abi[abi_index + 1]["name"]
@@ -52,21 +63,29 @@ def decode_parameters(parameters, parameters_abi):
         and decoded_parameters[2]["name"] == "calldata"
     ):
         # these parameters sometimes are a hex string but sometimes are felt
-        contract = hex(decoded_parameters[0]["value"]) \
-                   if type(decoded_parameters[0]["value"]) == int \
-                   else decoded_parameters[0]["value"]
-        selector = hex(decoded_parameters[1]["value"]) \
-                   if type(decoded_parameters[1]["value"]) == int \
-                   else decoded_parameters[1]["value"]
+        contract = (
+            hex(decoded_parameters[0]["value"])
+            if type(decoded_parameters[0]["value"]) == int
+            else decoded_parameters[0]["value"]
+        )
+        selector = (
+            hex(decoded_parameters[1]["value"])
+            if type(decoded_parameters[1]["value"]) == int
+            else decoded_parameters[1]["value"]
+        )
 
-        calldata = decoded_parameters[2]["value"] \
-                   if type(decoded_parameters[2]["value"]) == list \
-                   else [decoded_parameters[2]["value"]]
+        calldata = (
+            decoded_parameters[2]["value"]
+            if type(decoded_parameters[2]["value"]) == list
+            else [decoded_parameters[2]["value"]]
+        )
 
         semantics = get_semantics(contract)
         if semantics:
             function_abi = (
-                semantics["abi"]["functions"][selector] if selector in semantics["abi"]["functions"] else None
+                semantics["abi"]["functions"][selector]
+                if selector in semantics["abi"]["functions"]
+                else None
             )
             if function_abi:
                 function_name = function_abi["name"]
@@ -96,7 +115,12 @@ def create_parameters_string(parameters):
 def decode_struct(raw_values, members):
     values = []
     for i, member in enumerate(members):
-        values.append(dict(name=member['name'], value=decode_atomic_parameter(raw_values[i], member['type'])))
+        values.append(
+            dict(
+                name=member["name"],
+                value=decode_atomic_parameter(raw_values[i], member["type"]),
+            )
+        )
 
     return values
 
@@ -107,7 +131,9 @@ def decode_atomic_parameter(raw_value, parameter_type):
     elif parameter_type == "address":
         parameter_value = hex(int(raw_value))
     elif parameter_type == "string":
-        parameter_value = bytes.fromhex(hex(int(raw_value))[2:]).decode("utf-8").replace("\x00", "")
+        parameter_value = (
+            bytes.fromhex(hex(int(raw_value))[2:]).decode("utf-8").replace("\x00", "")
+        )
     else:
         parameter_value = raw_value
 
