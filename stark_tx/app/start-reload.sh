79d0726fb63c8bc893bf7e553d78c9a15fb3d4dd
Karol Chojnowski <karolchojnowski95@gmail.com>
2021-12-28 17:16:53 +0100

    updates

--- stark_tx/app/start-reload.sh
+++ stark_tx/app/start-reload.sh
@@ -1,8 +1,21 @@
 #! /usr/bin/env sh
 set -e
 
-# If there's a prestart.sh script in the /app/app directory, run it before starting
-PRE_START_PATH=/app/app/prestart.sh
+if [ -f /app/app/main.py ]; then
+    DEFAULT_MODULE_NAME=app.main
+elif [ -f /app/main.py ]; then
+    DEFAULT_MODULE_NAME=main
+fi
+MODULE_NAME=${MODULE_NAME:-$DEFAULT_MODULE_NAME}
+VARIABLE_NAME=${VARIABLE_NAME:-app}
+export APP_MODULE=${APP_MODULE:-"$MODULE_NAME:$VARIABLE_NAME"}
+
+HOST=${HOST:-0.0.0.0}
+PORT=${PORT:-80}
+LOG_LEVEL=${LOG_LEVEL:-info}
+
+# If there's a prestart.sh script in the /app directory or other path specified, run it before starting
+PRE_START_PATH=${PRE_START_PATH:-/app/prestart.sh}
 echo "Checking for script in $PRE_START_PATH"
 if [ -f $PRE_START_PATH ] ; then
     echo "Running script $PRE_START_PATH"
@@ -11,5 +24,5 @@ else
     echo "There is no script $PRE_START_PATH"
 fi
 
-# Start Gunicorn
-exec gunicorn "--reload" -k egg:meinheld#gunicorn_worker -c "$GUNICORN_CONF" "$APP_MODULE"
+# Start Uvicorn with live reload
+exec uvicorn --reload --host $HOST --port $PORT --log-level $LOG_LEVEL "$APP_MODULE"
