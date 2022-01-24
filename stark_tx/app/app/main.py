391673d2afdb58dfb0781ac562cb3238a63a64f9
Karol Chojnowski <karolchojnowski95@gmail.com>
2022-01-24 11:23:23 +0100

    typo

--- stark_tx/app/app/main.py
+++ stark_tx/app/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.routers.main import router
+from app.routes.main import router
 
 app = FastAPI()
 
