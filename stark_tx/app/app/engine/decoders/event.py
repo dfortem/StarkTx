9b1360578edf69a0b6d5dff4e82aedad678f35a4
tmierzwa <mierzwik@me.com>
2022-01-30 21:16:36 +0000

    anonymous events decoding

--- stark_tx/app/app/engine/decoders/event.py
+++ stark_tx/app/app/engine/decoders/event.py
@@ -5,7 +5,8 @@ def decode_event(chain_id, event, event_abi):
     if event_abi and 'parameters' in event_abi:
         parameters = decode_parameters(chain_id, event["data"], event_abi['parameters'])
     else:
-        parameters = [dict(name=f'data_{i}', value=event['data'][i]) for i in range(len(event['data']))]
+        parameters = [dict(name=f'key_{i}', value=event['keys'][i]) for i in range(len(event['keys']))] + \
+                     [dict(name=f'data_{i}', value=event['data'][i]) for i in range(len(event['data']))]
 
     decoded_event = dict(contract=event['from_address'], name=event_abi['name'] if event_abi else "anonymous",
                          keys=event['keys'], parameters=parameters)
