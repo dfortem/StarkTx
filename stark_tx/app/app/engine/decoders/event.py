50498af8af03122dda72edfcf580420aaf1fcfa4
tmierzwa <mierzwik@me.com>
2022-01-30 21:07:51 +0000

    anonymous events decoding

--- stark_tx/app/app/engine/decoders/event.py
+++ stark_tx/app/app/engine/decoders/event.py
@@ -2,8 +2,12 @@ from app.engine.decoders.parameter import decode_parameters
 
 
 def decode_event(chain_id, event, event_abi):
-    parameters = decode_parameters(chain_id, event["data"], event_abi['parameters'] if event_abi else [])
-    decoded_event = dict(contract=event['from_address'], name=event_abi['name'] if event_abi else "",
+    if event_abi and 'parameters' in event_abi:
+        parameters = decode_parameters(chain_id, event["data"], event_abi['parameters'])
+    else:
+        parameters = [dict(name=f'data_{i}', value=event['data'][i]) for i in range(len(event['data']))]
+
+    decoded_event = dict(contract=event['from_address'], name=event_abi['name'] if event_abi else "anonymous",
                          keys=event['keys'], parameters=parameters)
 
     return decoded_event
